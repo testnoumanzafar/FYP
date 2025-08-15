7a1bd70731d4e56c6893822237de27daa5ad4f3d
testnoumanzafar <mtj3310@gmail.com>
2025-08-14 20:41:23 -0700

    delete-in-group

--- src/constant/utils.jsx
+++ src/constant/utils.jsx
@@ -1,6 +1,6 @@
 //   const URL= http://localhost:4000
 export   const   URL="https://backend-fyp-1.onrender.com"
-
+// https://backend-fyp-1.onrender.com
 import { io } from "socket.io-client";
 const socket = io("https://backend-fyp-1.onrender.com",{
   transports: ["websocket"], // optional but helps avoid polling issues
